c363301d01f8ad783f7e9c5def4101004a8fe203
Swapnil Sagvekar <swapnil.spryox@gmail.com>
2017-09-20 17:28:17 +0530

    Changed homepage

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Learning</title>
+</head>
+<body>
+	
+	<h1>
+		Learning php
+	</h1>
 
-phpinfo();
+</body>
+</html>
