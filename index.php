b1ec1ad0d4a90cf5c1c78fc1a171964d78b2f4d1
Akshay Jumbade <akkyjumbade@gmail.com>
2017-09-20 18:21:57 +0530

    Removed html and added echo

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>PHP Learning</title>
-</head>
-<body>
-	
-	<h1>
-		Learning php, added new file here.
-	</h1>
+<?php
 
-</body>
-</html>
+echo "Hello world ";
